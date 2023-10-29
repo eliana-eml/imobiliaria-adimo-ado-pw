306008d9da054163e0818b65fe297c1525e0d6a2
eliana-eml <eml.eliana57@hotmail.com>
2023-10-29 15:13:02 -0300

    Criação do repositório para o projeto Imobiliária ADIMO de Programação Web

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("nav-mobile-lista").style.width = "270px";
+}
+
+function closeNav() {
+    document.getElementById("nav-mobile-lista").style.width = "0";
+}
